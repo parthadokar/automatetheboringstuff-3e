7a81d726770ccd4e0612ccdffaaf7d5564712c75
hoax <hoax@192.168.1.32>
2026-03-21 21:03:02 +0530

    first nvim commit

---
+++ Chapter-1/practice-1.py
@@ -0,0 +1,28 @@
+# Operator
+# String
+# Operator
+# Operator
+# Operator
+# Operator
+# Integer
+
+# spam = variable
+# 'spam' = string
+
+# Integer, String, Float, Boolean
+
+# Expressions are made up of combinations of values, data types , operators that evaluate to a value.
+
+# Expression Evaluate to a value, Statement Execute an action but do not produce a value
+
+# Bacon = 21
+
+# 'spamspamspam'
+# 'spamspamspam'
+
+# Egg follows the predefined notion of not having a special character or a number at its beginning.
+# 100 is an number which is against the convictions of variable naming in python.
+
+# int(),float(),str()
+
+# 'I eat ' + str(99) + ' burritos.'
