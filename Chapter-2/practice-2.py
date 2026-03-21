31b0219eff62a9f505ce57b48dd0b6e9d39027fc
Parth Adokar <adokar.parth@gmail.com>
2026-03-21 21:38:18 +0530

    second

---
+++ Chapter-2/practice-2.py
@@ -0,0 +1,26 @@
+# True
+# False
+
+# and , or and not
+
+# False
+# False
+# True
+# False
+# False
+# True
+
+# ==, !=, <, <=,>,>=
+
+# Equal to Operator Checks if both the values evaluates to equal or not.
+# Assignment Operator assings value to the variable on the left with the value on the right.
+
+# Condtion is something that is used to evaluate a certain scenirio to draw out conclusions
+
+spam = int(input('Enter a value for a result \n'))
+if spam == 1:
+    print('Hello')
+elif spam == 2:
+    print('Howdy')
+else:
+    print('Greetings')
